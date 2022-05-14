ac774f15f656d168f2fc4ee8357f55e15e4288cb
Cheryl <1030821728@qq.com>
2022-05-15 01:08:57 +0800

    update main logic
    将main函数更新为中间代码生成的逻辑，目标代码未做

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import os
 
 from parse import parser
+from intermediateCodeGenerator import IntCodeGen
 
 class MainForm():
     def __init__(self):
         self.AbstractSyntsxTreeRoot = None
-        self.IntermediateCode = None
+        self.IntermediateCode = IntCodeGen()
         self.TargetCode = None
         self.SourceCode = None
 
     def getIntermediateCode(self):
-        src_code = self.SourceCode.toPlainText()
+        src_code = self.SourceCode
         int_code = ""
         # if src_code == "":
         # # Some operation
         # else:
         self.AbstractSyntsxTreeRoot = parser.parse(src_code)
+        self.IntermediateCode.initial(self.AbstractSyntsxTreeRoot)
+        int_code = self.IntermediateCode.Generation()
         return int_code
 
     def Compile(self):
         int_code = self.getIntermediateCode()
-
+        print(int_code)
         #TODO: 生成目标代码
 
         #TODO: 写入二进制文件
@@ -41,3 +44,8 @@ class MainForm():
 
 if __name__ == "__main__":
     print("main")
+    mainForm = MainForm()
+    f = open('./Test/TestCase3.pas', 'r', encoding='utf-8')
+    mainForm.SourceCode = f.read()
+    f.close()
+    mainForm.Compile()
